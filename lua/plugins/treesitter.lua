fe33450d1da7e38a9dca4e94fed64decb69a60b0
gary <gary@canopywave.com>
2025-10-24 14:28:22 +0800

    update: treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      ensure_installed = {"bash","python", "javascript", "go"},
+      highlight = { enable = true },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true, -- 自动前瞻
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+          },
+        },
+      },
+    }
+  end,
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects"
+  }
+}
